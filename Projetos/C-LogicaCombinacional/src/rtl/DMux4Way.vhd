9f9d1b658568760371571fc664ec84d29297cb68
WarlenRodrigues <warlencrf@al.insper.edu.br>
2018-03-14 17:52:41 -0300

    Comentei o código do DMux4Way

--- Projetos/C-LogicaCombinacional/src/rtl/DMux4Way.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/DMux4Way.vhd
@@ -14,7 +14,7 @@ end entity;
 ------------------------------------------------------
 architecture rtl of DMux4Way is
 begin
-
+--Para fazer o DMux eu preferi implementar a Equação da tabela verdade do que definir as condições para as saídas
  q0 <= ((not sel(1)) and (not sel(0)) and a);
  q1 <= ((not sel(1)) and sel(0) and a);
  q2 <= (sel(1) and (not sel(0)) and a);
