55b1ee9a87f754ba9af6fb1e2d41306d00d20c8d
WarlenRodrigues <warlencrf@al.insper.edu.br>
2018-03-14 17:37:43 -0300

    Comentei o código do OR16

--- Projetos/C-LogicaCombinacional/src/rtl/Or16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/Or16.vhd
@@ -1,6 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+ 
 entity Or16 is
 	port ( 
 			a:   in  STD_LOGIC_VECTOR(15 downto 0);
@@ -12,7 +12,7 @@ end entity;
 
 architecture rtl of OR16 is
 begin
-
+	--Apenas determinei que a saída "q" terá o valor de "a or b". Um OR bem simples.
 	q <= (a or b);
 
 end rtl;
