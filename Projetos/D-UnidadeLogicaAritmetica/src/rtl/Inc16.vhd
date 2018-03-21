ba89a48e259c072e6bbb881f734450fd919fe64c
WarlenRodrigues <warlencrf@al.insper.edu.br>
2018-03-21 16:55:18 -0300

    Inc16 funcionando e testado

--- Projetos/D-UnidadeLogicaAritmetica/src/rtl/Inc16.vhd
+++ Projetos/D-UnidadeLogicaAritmetica/src/rtl/Inc16.vhd
@@ -8,22 +8,23 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
-entity Inc16 is
+entity Inc16 is --Criando o Inc16
 	port(
 		a   :  in STD_LOGIC_VECTOR(15 downto 0);
 		q   : out STD_LOGIC_VECTOR(15 downto 0)
 	);
 end entity;
 
-architecture rtl of Inc16 is
+architecture rtl of Inc16 is --Definindo a arquitetura do Inc16
 
-component HalfAdder is
+component Add16 is --Chamando o Add16 para usar com o port map mais abaixo
 	port(
-		a,b:        in STD_LOGIC;   -- entradas
-		soma,vaium: out STD_LOGIC   -- sum e carry
+		a,b   :  in STD_LOGIC_VECTOR(15 downto 0);
+		q   : out STD_LOGIC_VECTOR(15 downto 0)
 	);
 end component;
 
 begin
-
+	
+	j : Add16 port map (a,"0000000000000001",q); --Criando o Inc16 com uso o Add16 colocando "1" no último bit do vetor b
 end architecture;
