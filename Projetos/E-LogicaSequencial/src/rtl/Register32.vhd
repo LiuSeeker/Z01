f4281e3f221c939eda8f4ca61c854c0130ff2933
LiuSeeker <vitor.liu@gmail.com>
2018-03-28 11:41:56 -0300

    Register32 funcionando (e Reg64 tb)

--- Projetos/E-LogicaSequencial/src/rtl/Register32.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/Register32.vhd
@@ -1,4 +1,4 @@
--- Elementos de Sistemas
+	-- Elementos de Sistemas
 -- by Luciano Soares
 -- Register32.vhd
 
@@ -13,3 +13,25 @@ entity Register32 is
 		output: out STD_LOGIC_VECTOR(31 downto 0)
 	);
 end entity;
+
+architecture rtl of Register32 is
+
+	component Register16 is
+		port(
+			clock:   in STD_LOGIC;
+			input:   in STD_LOGIC_VECTOR(15 downto 0);
+			load:    in STD_LOGIC;
+			output: out STD_LOGIC_VECTOR(15 downto 0)
+			);
+	end component;
+
+	signal s1, s2: STD_LOGIC_VECTOR(15 downto 0):= (others => '0');
+
+	begin
+
+	r1: Register16 port map (clock,input(31 downto 16),load,s1);
+	r2: Register16 port map (clock,input(15 downto 0),load,s2);
+	output <= s1 & s2;
+
+end architecture;
+
